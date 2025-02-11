332b58add0c3f04958ea076d9f36a2f416900191
Jonathan Moussa <jemoussa@vt.edu>
2025-02-11 17:02:33 -0500

    Turn off testing & all Python dependence

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,11 +3,9 @@ cmake -Bbuild ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DGIT_HASH=OFF ^
-      -DPython3_EXECUTABLE=%PYTHON%
+      -DTESTS=OFF
 if errorlevel 1 exit 1
 cmake --build build
 if errorlevel 1 exit 1
 cmake --install build
-if errorlevel 1 exit 1
-ctest -VV --parallel %CPU_COUNT%
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,7 @@ cmake -Bbuild -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBLA_VENDOR=Generic \
   -DGIT_HASH=OFF \
-  -DPython3_EXECUTABLE=${PYTHON}
+  -DTESTS=OFF
 
 cmake --build build
 cmake --install build
-ctest -VV --parallel ${CPU_COUNT}
